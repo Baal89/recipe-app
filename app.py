16dda03033fa85946913ecc1e54ae70b12172641
Ubuntu <ubuntu@ip-172-31-24-252.eu-west-1.compute.internal>
2019-11-18 17:15:13 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'recipes_app'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_recipes')
+def get_recipe():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
+    
+if __name__=='__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
